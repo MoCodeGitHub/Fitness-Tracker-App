73406c6d3146079d2841ecb5e29c79a114abea98
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2020-01-07 20:13:27 +0100

    Navbar set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function App() {
   return (
+    <router>
     <div className="container">
       Fitness Tracker
     </div>
+    </router>
   );
 }
 
