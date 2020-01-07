32b2ecf4566ecac57740726ca8cba90a10c5c205
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2020-01-07 20:59:25 +0100

    add constructor

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 export default class CreateExercise extends Component {
+    constructor(props) {
+        super(props):
+
+        this.state = {
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+
+        } 
+
+        }
+    }
     render (){
         return (
            <div>
