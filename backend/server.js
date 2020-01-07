63cb526a2fceb52ea926d922765b93a3d362998b
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2020-01-07 18:20:06 +0100

    connect to the database

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,15 @@ const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { 
     useNewUrlParser: true, 
     useCreateIndex: true, 
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
 });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
+
 })
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
