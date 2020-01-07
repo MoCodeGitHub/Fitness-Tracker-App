ef861616fdf1f46e8fbcca8d168b9ca90e9ace88
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2020-01-07 16:40:46 +0100

    Fix Server Discovery & Monitoring engine deprecated status

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,23 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreatIndex: true});
+mongoose.connect(uri, { 
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    useUnifiedTopology: true,
+});
 
-const connection = mongoose.connection; 
+const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully")
+  console.log("MongoDB database connection established successfully");
 })
 
 app.listen(port, () => {
-    console.log('Server is running on port: ${port}');
-}
+    console.log(`Server is running on port: ${port}`);
+});
