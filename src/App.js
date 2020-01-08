e5466aca183b8bcc4c3bd430c02e066fe2cec4ff
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2020-01-08 23:00:26 +0100

    rm import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import image from './image.png'
 
 
 
