29fcdae1d49c44ede8a16f1dd45af46c595cc44e
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2020-01-08 20:14:28 +0100

    Edit Exercise functionality added

--- src/components/EditExercise.js
+++ src/components/EditExercise.js
@@ -88,7 +88,7 @@ onSubmit(e){
 
     console.log(exercise);
 
-    axios.post('http://localhost:5000/exercises/add', exercise)
+    axios.post('http://localhost:5000/exercises/update', exercise)
       .then(res => console.log(res.data));
 
     window.location = '/';
