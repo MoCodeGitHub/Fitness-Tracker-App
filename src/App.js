31f6f5c7c5fd75df30e666983fa21472f0640442
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2020-01-08 22:51:04 +0100

    new background

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import image from './image.png'
+
+
 
 import Navbar from "./components/navbar"
 import ExercisesList from "./components/ExercisesList";
@@ -12,17 +15,19 @@ import CreateUsers from "./components/CreateUsers";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-      <Navbar />
-      <br/>
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/users" component={CreateUsers} />
-      </div>
-    </Router>
+  <Router>
+  <div class="bg-img">
+  <div className="container">
+  <Navbar />
+  <br/>
+  <Route path="/" exact component={ExercisesList} />
+  <Route path="/edit/:id" component={EditExercise} />
+  <Route path="/create" component={CreateExercise} />
+  <Route path="/users" component={CreateUsers} />
+  </div>
+  </div>
+  </Router>
   );
-}
+  }
 
 export default App;
