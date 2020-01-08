586697a6e7fe27d4c9e307b680e66c96cd01810b
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2020-01-08 19:47:34 +0100

    Add template

--- src/components/ExercisesList.js
+++ src/components/ExercisesList.js
@@ -31,11 +31,25 @@ deleteExercise(id) {
     })
   }
 
-    render (){
-        return (
-           <div>
-               <p>Exercises List</p>
-           </div> 
-         )
-    }
+  render() {
+    return (
+      <div>
+        <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.exercisesList() }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
