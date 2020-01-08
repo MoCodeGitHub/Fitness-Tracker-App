47914ff227340362cdab114e5aa129ee3f1d9a76
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2020-01-08 18:09:28 +0100

    Create User functionality added and tested

--- src/components/CreateUsers.js
+++ src/components/CreateUsers.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 
 export default class CreateUsers extends Component {
-    render (){
-        return (
-           <div>
-               <p>Create Users</p>
-           </div> 
-         )
+    constructor(props) {
+        super(props);
+    
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    
+        this.state = {
+          username: '',
+        }
+      }
+
+onChangeUsername(e){
+    this.setState ({
+         username: e.target.value
+         })
     }
-}
+
+onSubmit(e){
+    e.preventDefault();
+    
+    const users = {
+        username: this.state.username
+
+    }
+
+console.log(users);
+
+    this.setState({
+      username: ''
+    })
+  }
+
+
+
+    render() {
+        return (
+          <div>
+            <h3>Create New User</h3>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group"> 
+                <label>Username: </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.username}
+                    onChange={this.onChangeUsername}
+                    />
+              </div>
+              <div className="form-group">
+                <input type="submit" value="Create User" className="btn btn-primary" />
+              </div>
+            </form>
+          </div>
+        )
+      }
+    }
